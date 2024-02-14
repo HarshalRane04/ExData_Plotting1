73dc647ee88f6fc4c610f6912183d44840786dfe
HarshalRane04 <85073947+HarshalRane04@users.noreply.github.com>
2024-02-15 02:29:21 +0530

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,44 +1,19 @@
-library("data.table")
-
-setwd("C:/Users/harsh/Desktop/R Projects/exdata_data_household_power_consumption")
-
-#Reads in data 
-powerData <- data.table::fread(input = "household_power_consumption.txt"
-                             , na.strings="?"
-)
-
-
-powerData[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
-
-
-powerData[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
-
-# Filter Dates 
-powerData <- powerData[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
-
+data <- read.csv("household_power_consumption.txt", sep=";")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+globalActivePower <- as.numeric(subSetData$Global_active_power)
+globalReactivePower <- as.numeric(subSetData$Global_reactive_power)
+voltage <- as.numeric(subSetData$Voltage)
+subMetering1 <- as.numeric(subSetData$Sub_metering_1)
+subMetering2 <- as.numeric(subSetData$Sub_metering_2)
+subMetering3 <- as.numeric(subSetData$Sub_metering_3)
 png("plot4.png", width=480, height=480)
-
-par(mfrow=c(2,2))
-
-# Plot 1
-plot(powerData[, dateTime], powerData[, Global_active_power], type="l", xlab="", ylab="Global Active Power")
-
-# Plot 2
-plot(powerData[, dateTime],powerData[, Voltage], type="l", xlab="datetime", ylab="Voltage")
-
-# Plot 3
-plot(powerData[, dateTime], powerData[, Sub_metering_1], type="l", xlab="", ylab="Energy sub metering")
-lines(powerData[, dateTime], powerData[, Sub_metering_2], col="red")
-lines(powerData[, dateTime], powerData[, Sub_metering_3],col="blue")
-legend("topright", col=c("black","red","blue")
-       , c("Sub_metering_1  ","Sub_metering_2  ", "Sub_metering_3  ")
-       , lty=c(1,1)
-       , bty="n"
-       , cex=.5) 
-
-# Plot 4
-plot(powerData[, dateTime], powerData[,Global_reactive_power], type="l", xlab="datetime", ylab="Global_reactive_power")
-
+par(mfrow = c(2, 2))
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
 dev.off()
-
-
