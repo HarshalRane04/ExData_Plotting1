8b394b7fdc77d227984d6fe5b47d0177be449d22
HarshalRane04 <85073947+HarshalRane04@users.noreply.github.com>
2024-02-15 02:27:13 +0530

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,25 +1,7 @@
-library("data.table")
-
-setwd("C:/Users/harsh/Desktop/R Projects/exdata_data_household_power_consumption")
-
-#Reads in data
-powerData <- data.table::fread(input = "household_power_consumption.txt"
-                             , na.strings="?"
-)
-
-powerData[, Global_active_power := lapply(.SD, as.numeric), .SDcols = c("Global_active_power")]
-
-
-powerData[, dateTime := as.POSIXct(paste(Date, Time), format = "%d/%m/%Y %H:%M:%S")]
-
-# Filter Dates 
-powerData <- powerData[(dateTime >= "2007-02-01") & (dateTime < "2007-02-03")]
-
+data <- read.csv("household_power_consumption.txt", sep=";")
+subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+datetime <- strptime(paste(subSetData$Date, subSetData$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+globalActivePower <- as.numeric(subSetData$Global_active_power)
 png("plot2.png", width=480, height=480)
-
-## Plot 2
-plot(x = powerData[, dateTime]
-     , y = powerData[, Global_active_power]
-     , type="l", xlab="", ylab="Global Active Power (kilowatts)")
-
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
